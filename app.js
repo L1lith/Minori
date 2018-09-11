2a462c8f56fc954417ace7cc8245d900fb19dbf6
L1lith <lily@lillith.pw>
2018-09-11 14:38:14 -0600

    add help status

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const token = config.token
 const client = new Discord.Client()
 
 client.on('ready', () => {
-  console.log('I am ready!')
+  console.log('I am awake!')
+  client.user.setGame('Try "Minori Help"')
 })
 
 client.on('message', messageListener.bind(null, client))
