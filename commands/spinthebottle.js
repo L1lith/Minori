1b7eb72b661f856532ec44919c2333d53e7c3c85
L1lith <lily@lillith.pw>
2018-09-19 00:08:45 -0600

    fix up the spinthebottle command

--- commands/spinthebottle.js
+++ commands/spinthebottle.js
@@ -1,8 +1,15 @@
 const kissImage = "https://i.imgur.com/Muh7YYP.jpg"
 
-async function spinTheBottle(message) {
+async function spinTheBottle(message, args, {client}) {
   const members = Array.from(message.guild.members)
-  const match = members[Math.floor(Math.random()*members.length)][0]
+  if (members.length < 3) return "Not enough players"
+  let match = null
+  let tries = 0
+  while (match === null || match === client.user.id || match === message.author.id) {
+    tries++
+    if (tries > 5) return "Picking error"
+    match = members[Math.floor(Math.random()*members.length)][0]
+  }
   return `<@${match}> has been selected as your victim.\n${kissImage}`
 }
 
